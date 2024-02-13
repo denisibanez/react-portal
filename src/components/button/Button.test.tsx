349a2ff81ce30d5d2592788e8854532b46be2b65
denis.ibanez@outlook.com <denis.ibanez@outlook.com>
2024-02-13 20:20:56 +0000

    insert react, vite, ts, scss

--- src/components/button/Button.test.tsx
+++ src/components/button/Button.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import ButtonComponent from './Button';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello React/i);
+  render(<ButtonComponent />);
+  const linkElement = screen.getByText(/Cont/i);
   expect(linkElement).toBeInTheDocument();
 });
